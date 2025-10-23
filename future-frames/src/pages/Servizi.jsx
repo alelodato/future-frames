739623bf760f9a8bc8e2f2916c54c0c7be2b6bba
alelodato <alss.ldt@gmail.com>
2025-10-23 17:27:01 +0700

    styled eventi section

--- future-frames/src/pages/Servizi.jsx
+++ future-frames/src/pages/Servizi.jsx
@@ -18,17 +18,23 @@ export default function Servizi() {
       <section className={styles.servizi}>
         <div className={styles.eventi}>
           <div className={styles.gradient}>
-            <h3>DAL BACKSTAGE AL MOMENTO CLOU:</h3>
+            <h3>
+              DAL <strong>BACKSTAGE</strong> AL <strong>MOMENTO</strong> CLOU:
+            </h3>
             <h4>
-              raccontiamo l'energia dei tuoi eventi con immagini che coinvolgono
-              e restano.
+              Raccontiamo l'<strong>ENERGIA</strong> dei tuoi eventi con{" "}
+              <strong>IMMAGINI</strong> che coinvolgono e restano.
             </h4>
-
-            <p>
-              Fotografia e video per eventi privati e aziendali. Copertura
-              completa con scatti spontanei e riprese dinamiche. Ideale per
-              feste, lanci di prodotto, eventi culturali, convention.
-            </p>
+            <div className={styles.eventip}>
+              <h5 className={styles.eventip1}>Fotografia e video per eventi privati e aziendali.</h5>
+              <h5 className={styles.eventip2}>
+                Copertura completa con scatti spontanei e riprese dinamiche.
+              </h5>
+              <h5 className={styles.eventip3}>
+                Ideale per feste, lanci di prodotto, eventi culturali,
+                convention.
+              </h5>{" "}
+            </div>
           </div>
         </div>
         <div className={styles.aziende}>
@@ -37,24 +43,37 @@ export default function Servizi() {
               Contenuti visivi su misura che rafforzano l'identità del tuo brand
               e parlano direttamente al tuo pubblico.
             </h3>
-            <p>
+            <h4>
               Raccontiamo la tua azienda con immagini che non si limitano a
-              mostrare, ma comunicano valore. Dai ritratti corporate ai video
-              istituzionali, fino agli spotpubblicitari e ai contenuti social,
-              ogni progetto è studiato per valorizzare la tua identità e
-              distinguerti sul mercato.
+              mostrare, ma comunicano valore.
+            </h4>
+            <p className={styles.aziendep}>
+              {" "}
+              Dai ritratti corporate ai video istituzionali, fino agli
+              spotpubblicitari e ai contenuti social, ogni progetto è studiato
+              per valorizzare la tua identità e distinguerti sul mercato.
             </p>
             <h4>Cosa possiamo realizzare per te:</h4>
-            <p>
+            <h5 className={styles.istituzionali}>
               Video istituzionali e corporate: per presentare l'azienda, i
-              valori e le persone dietro al brand. Spot pubblicitari e social
-              video: contenuti brevi e di impatto per campagne e distribuzione
-              digitale. Ritratti e fotografia aziendale: immagini professionali
-              di team, sedi, prodotti e processi. Contenuti per employer
-              branding: video e foto che raccontano la cultura aziendale e
-              attraggono talenti. Shooting prodotto/servizio: still-life e foto
-              ambientate per cataloghi, e-commerce e materiale promozionale.
-            </p>
+              valori e le persone dietro al brand.
+            </h5>
+            <h5 className={styles.spot}>
+              Spot pubblicitari e social video: contenuti brevi e di impatto per
+              campagne e distribuzione digitale.
+            </h5>
+            <h5 className={styles.ritratti}>
+              Ritratti e fotografia aziendale: immagini professionali di team,
+              sedi, prodotti e processi.
+            </h5>
+            <h5 className={styles.branding}>
+              Contenuti per employer branding: video e foto che raccontano la
+              cultura aziendale e attraggono talenti.
+            </h5>
+            <h5 className={styles.shooting}>
+              Shooting prodotto/servizio: still-life e foto ambientate per
+              cataloghi, e-commerce e materiale promozionale.
+            </h5>
           </div>
         </div>
         <div className={styles.cerimonie}>
