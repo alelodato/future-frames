f7c52a03def7444cee4d5804acb692b498b95651
alelodato <alss.ldt@gmail.com>
2025-10-23 14:24:07 +0700

    started servizi page

--- future-frames/src/pages/Servizi.jsx
+++ future-frames/src/pages/Servizi.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import styles from "./Servizi.module.css";
 
-function Servizi() {
+export default function Servizi() {
   return (
-    <div>Servizi</div>
-  )
+    <div className={styles.wrap}>
+      <div className={styles.title}>
+        <h1>I NOSTRI SERVIZI</h1>
+      </div>
+      <div className={styles.intro}>
+        <p className={styles.introP2}>
+          Con<strong> CREATIVITA'</strong>,<strong> QUALITA' PROFESSIONALE</strong> e
+          <strong> MASSIMA AFFIDABILITA'</strong>, accompagniamo ogni passaggio —
+          dalla prima ripresa al risultato finale
+        </p>
+        <p className={styles.introP3}>
+          Scopri come possiamo valorizzare anche il tuo progetto, un frame alla
+          volta.
+        </p>
+      </div>
+    </div>
+  );
 }
-
-export default Servizi
